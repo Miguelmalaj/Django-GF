2561d761df504b3a2476379f675b4db7a7ff4ecd
Miguelmlj <hsmiguelhs@gmail.com>
2023-09-26 18:35:46 -0700

    generacion de reportes PDF de tarjetas cuentas x cobrar y cuentas x pagar

--- djtableros/app_contabilidad/urls.py
+++ djtableros/app_contabilidad/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cuentasxcobrar/', views.cuentasxcobrar, name='cuentasxcobrar'),
-    path('cuentasxcobrar/pdf/', views.generatepdf_cuentasxcobrar, name='pdf_cuentasxcobrar'),
+    path('cuentasxcobrar/pdf/', views.cuentasxcobrarpdf, name='cuentasxcobrarpdf'),
     path('cuentasxcobrar_detalle/<str:empresa>/<str:sucursal>', views.cuentasxcobrar_detalle, name='cuentasxcobrar_detalle'),
     path('cuentasxcobrar_detalle/pdf/<str:empresa>/<str:sucursal>', views.cuentasxcobrarpdf_detalle, name='cuentasxcobrarpdf_detalle'),
     path('cuentasxpagar', views.cuentasxpagar, name='cuentasxpagar'),
+    path('cuentasxpagar/pdf/', views.cuentasxpagarpdf, name='cuentasxpagarpdf'),
     path('cuentasxpagar_detalle/<str:empresa>/<str:sucursal>', views.cuentasxpagar_detalle, name='cuentasxpagar_detalle'),
     path('cuentasxpagar_detalle/pdf/<str:empresa>/<str:sucursal>', views.cuentasxpagarpdf_detalle, name='cuentasxpagarpdf_detalle'),
 ]
--- djtableros/app_contabilidad/views.py
+++ djtableros/app_contabilidad/views.py
@@ -97,7 +97,7 @@ def cuentasxcobrarpdf_detalle(request, empresa, sucursal):
         response['Content-Disposition'] = 'attachment; filename="cuentasxcobrar_detalles.pdf"'
         return response
 
-def generatepdf_cuentasxcobrar(request):
+def cuentasxcobrarpdf(request):
 
         datoslmm = consultas.resumen_cuentasxcobrar(1, 1)                      
         datosgve = consultas.resumen_cuentasxcobrar(3, 1)
@@ -126,53 +126,30 @@ def generatepdf_cuentasxcobrar(request):
                 'cadillac': datoscad
         }
 
-        rendered_template = render(request, 'templatetest.html', {'datos':datos}) 
-        # rendered_template = render(request, 'cuentasxcobrar.html', {'datos':datos}) 
-        # rendered_template = render(request, 'base.html') 
 
+        rendered_template = render(request, 'cuentasxcobrarpdf.html', {'datos':datos}) 
         
         with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as temp_html:
             temp_html.write(rendered_template.content)
 
-        # Path to the HTML template file
-        # html_template = os.path.join(os.path.dirname(__file__), 'templates', 'base.html')
-        #       html_template = os.path.dirname(__file__) + '/templates/base.html'
-
-
-        # Output file path for the PDF
-        # pdf_file = os.path.dirname(__file__) + '/docs/output.pdf'
-
-        # Configure options for wkhtmltopdf (optional)
-        # 'page-size': 'letter',
-
-        
         options = {
-                'page-size': 'A4',
-                'margin-top': '0mm',
+                'page-size': 'letter',
+                'margin-top': '12mm',
                 'margin-right': '0mm',
                 'margin-bottom': '0mm',
                 'margin-left': '0mm',
         }
-
-        # Convert HTML to PDF using pdfkit
-        # pdfkit.from_file(html_template, pdf_file, options=options)
-        # pdfkit.from_file(rendered_template, pdf_file, options=options)
-
         
         pdf_filename = tempfile.mktemp(suffix='.pdf')
-        pdfkit.from_file(temp_html.name, pdf_filename)
-        # pdf = pdfkit.from_file(rendered_template, False)
-
+        pdfkit.from_file(temp_html.name, pdf_filename, options = options ) #, options = options
         
         os.remove(temp_html.name)
-
-        
 
         # Create an HTTP response with the PDF content
         with open(pdf_filename, 'rb') as pdf_file:
           response = HttpResponse(pdf_file.read(), content_type='application/pdf')
         
-        response['Content-Disposition'] = 'attachment; filename="output.pdf"'
+        response['Content-Disposition'] = 'attachment; filename="Reporte_Cuentasxcobrar.pdf"'
         return response
 
 def cuentasxpagar(request):
@@ -203,6 +180,58 @@ def cuentasxpagar(request):
                 'cadillac': datoscad
         }
         return render(request, 'cuentasxpagar.html', {'datos':datos})
+
+def cuentasxpagarpdf(request):
+        datoslmm = consultas.resumen_cuentasxpagar(1, 1)                      
+        datosgve = consultas.resumen_cuentasxpagar(3, 1)
+        datoscln = consultas.resumen_cuentasxpagar(5, 1)
+        datosaer = consultas.resumen_cuentasxpagar(5, 2)
+        datoscad = consultas.resumen_cuentasxpagar(7, 1)
+
+        # Lista de las cuatro variables para iterar
+        variables = [datoslmm, datosgve, datoscln, datoscad]
+        # Llave a comparar
+        llave = 'diasmas'
+        # Inicializa el valor máximo con un valor muy pequeño
+        valor_mas_antiguo = float('-inf')
+        # Itera a través de las variables y encuentra el valor más grande en la llave 'diasmas'
+        for variable in variables:
+                if llave in variable and variable[llave] > valor_mas_antiguo:
+                        valor_mas_antiguo = variable[llave]
+
+        datos = {
+                'opcionmenu': utils.obtiene_opcionmenu('cuentasxpagar'),
+                'mochis': datoslmm,
+                'guasave': datosgve,
+                'culiacan': datoscln,
+                'aeropuerto': datosaer,
+                'cadillac': datoscad
+        }
+
+        rendered_template = render(request, 'cuentasxpagarpdf.html', {'datos':datos})
+
+        with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as temp_html:
+               temp_html.write(rendered_template.content)
+
+        options = {
+               'page-size': 'letter',
+               'margin-top': '12mm',
+               'margin-right': '0mm',
+               'margin-bottom': '0mm',
+               'margin-left': '0mm',
+        }
+
+        pdf_filename = tempfile.mktemp(suffix='.pdf')
+        pdfkit.from_file(temp_html.name, pdf_filename, options= options)
+
+        os.remove(temp_html.name)
+
+        with open(pdf_filename, 'rb') as pdf_file:
+               response = HttpResponse(pdf_file.read(), content_type='application/pdf')
+
+        response['Content-Disposition'] = 'attachment; filename="Reporte_Cuentasxpagar.pdf"'
+        return response
+               
 
 def cuentasxpagar_detalle(request, empresa, sucursal):
         agencia = int(empresa)
